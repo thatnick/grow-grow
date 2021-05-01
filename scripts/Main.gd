8b24e9ed42da969693b00d0e2165c62eb33e942d
Nicholas Russell <nick@spirited.software>
2021-05-01 17:59:43 +0100

    Collectables auto spawn and respawn

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,26 +1,24 @@
 extends Node
 
-#const NUM_COLLECTABLES = 20
-#var collectable = preload("res://scenes/Collectable.tscn")
-#onready var background_extents = $Background/CollisionShape2D.shape.extents
-#onready var background_centre = $Background/CollisionShape2D.position
-#onready var test = $Background/Sprite.get_rect()
-#
-#func _ready():
-#	randomize()
-#	print("rect is " + str(test))
-#	for i in NUM_COLLECTABLES:
-#		spawn(collectable)
-#
-#func _on_Collectable_collected(value):
-#	spawn(collectable)
-#
-#func spawn(scene):
-#	var new_collectable = scene.instance()
-#	var pos_in_extents = Vector2(
-#		(randi() % background_extents.x) - (background_extents.x/2) + background_centre.x,
-#		(randi() % background_extents.y) - (background_extents.y/2) + background_centre.y
-#	)
-#
-#	new_collectable.position = pos_in_extents
-#	add_child(new_collectable)
+const NUM_COLLECTABLES = 20
+const background_size = Vector2(1280, 1280)
+const collectable = preload("res://scenes/Collectable.tscn")
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	rng.randomize()
+	for i in NUM_COLLECTABLES:
+		spawn(collectable)
+
+func _on_Collectable_collected(value):
+	spawn(collectable)
+
+func spawn(scene):
+	var new_collectable = scene.instance()
+	new_collectable.connect("collected", self, "_on_Collectable_collected")
+	new_collectable.connect("collected", $Player, "_on_Collectable_collected")
+	new_collectable.position = Vector2(
+		rng.randi_range(0, background_size.x),
+		rng.randi_range(0, background_size.y)
+	)
+	add_child(new_collectable)
