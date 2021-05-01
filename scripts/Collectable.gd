f3cf36232acb817687c3acbad4a2cf4da65cb06a
Nicholas Russell <nick@spirited.software>
2021-05-01 14:26:20 +0100

    Tidy up naming

--- scripts/Collectable.gd
+++ scripts/Collectable.gd
@@ -5,6 +5,6 @@ signal collected(value)
 var value = 1
 
 func _on_Collectable_body_entered(body):
-	if body.name == "PhysicHead":
+	if body.name == "Head":
 		emit_signal("collected", value)
 		queue_free()
