1022c13d8abd1951e28d1c809f7e7ef9cff01681
Nicholas Russell <nick@spirited.software>
2021-05-01 10:57:00 +0100

    Add tails on collect (not fully working!) and set main scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,18 @@
 extends Node
 
+var tail = preload("res://scenes/Tail.tscn")
+
 func _physics_process(delta):
 	pass
 	
 func _on_Collectable_collected(value):
+	print("collected thing with value: " + str(value))
 	add_tail()
 
 func add_tail():
 	print("added tail")
-	
+	var new_tail = tail.instance()
+	var last_tail = get_child(get_child_count()-1)
+	print(str(last_tail.name))
+	new_tail.transform.origin = last_tail.transform.origin - Vector2(50, 50)
+	$PhysicHead.add_child_below_node(last_tail, new_tail)
