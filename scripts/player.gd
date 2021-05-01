268839cb9ec7e68de539fa66c05f46713fa1fe30
Alistair <32274829+plyval@users.noreply.github.com>
2021-05-01 12:35:29 +0100

    Fix adding tail

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends Node
 var tail = preload("res://scenes/Tail.tscn")
 
 #ideally we would want to work out the offset to be the radius of the collision shape of the new_tail plus the 0
-const offset = 32
+#const offset = 32
 
 
 func _physics_process(delta):
@@ -18,5 +18,7 @@ func add_tail():
 	var new_tail = tail.instance()
 	var last_tail = get_child(get_child_count() - 1)
 	print(str(last_tail.name))
-	new_tail.position = last_tail.position
+	var offset = Vector2(cos(last_tail.rotation + PI), sin(last_tail.rotation +PI)) * 32
+	print(offset)
+	new_tail.position = last_tail.position + offset
 	add_child(new_tail)
