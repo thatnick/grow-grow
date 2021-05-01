b1e4da46134cd19f1d9fda3790386bb57de13e64
Nicholas Russell <nick@spirited.software>
2021-05-01 09:19:31 +0100

    Normalized head velocity

--- scripts/PhysicHead.gd
+++ scripts/PhysicHead.gd
@@ -7,6 +7,6 @@ func _physics_process(delta):
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
-	velocity = move_and_slide(velocity * SPEED)
+	velocity = move_and_slide(velocity.normalized() * SPEED)
 
 
