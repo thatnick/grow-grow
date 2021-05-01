c8370c8e8d6bcb2251b4ab88360596856c9429ea
Nicholas Russell <nick@spirited.software>
2021-05-01 12:42:46 +0100

    Added check that collision with Collectable was the Head

--- scripts/Collectable.gd
+++ scripts/Collectable.gd
@@ -5,6 +5,6 @@ signal collected(value)
 var value = 1
 
 func _on_Collectable_body_entered(body):
-	print(str(body.name))
-	emit_signal("collected", value)
-	queue_free()
+	if body.name == "PhysicHead":
+		emit_signal("collected", value)
+		queue_free()
