60233db297c31911f99403671c14f8345dc1c972
Alistair <32274829+plyval@users.noreply.github.com>
2021-05-01 12:03:07 +0100

    head rotation added but buggy

--- scripts/Head.gd
+++ scripts/Head.gd
@@ -1,10 +1,14 @@
 extends KinematicBody2D
 
 var velocity = Vector2(0, 0)
+var direction = Vector2(0,0)
 const SPEED = 100
 
 func _physics_process(delta):
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
+	direction = position + velocity
+	look_at(direction)
+	print(direction)
 	velocity = move_and_slide(velocity.normalized() * SPEED)
