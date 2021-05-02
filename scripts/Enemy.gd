471c8be0272a8a10596e4b09d97dc83f08eace12
Nicholas Russell <nick@spirited.software>
2021-05-02 12:57:42 +0100

    Enemies now spawn outside the screen

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -10,19 +10,17 @@ func _ready():
 func _physics_process(delta):
 	rotation = velocity.angle()
 	var collision = move_and_collide(velocity * delta)
-	
+
 	if collision && collision.collider.has_method("hit"):
 		queue_free()
 		collision.collider.hit()
 		return
-		
+
 	if collision:
 		velocity = velocity.bounce(collision.normal)
 
 func _on_VisibilityNotifier2D_screen_entered():
-	pass
-	#$CollisionShape2D.disabled = false
+	$CollisionShape2D.disabled = false
 
 func on_spawn():
 	$CollisionShape2D.disabled = true
-	velocity = Vector2(speed, -1).rotated(randi() % int(PI * 2))
