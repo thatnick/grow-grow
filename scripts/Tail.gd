e0eadadbaabba0f43db4648fea96ebae1b8a33a0
Nicholas Russell <nick@spirited.software>
2021-05-02 07:15:49 +0100

    Fix starting angle of tail pieces (which I broke)

--- scripts/Tail.gd
+++ scripts/Tail.gd
@@ -2,26 +2,22 @@ extends KinematicBody2D
 
 const GAP = 32
 
-var parent_pos = Vector2()
-var pos = Vector2()
-var direction = Vector2()
-var velocity = Vector2(0,0)
+func _ready():
+	var next_child_up = get_parent().get_child(self.get_position_in_parent() - 1)
+	var direction = position.direction_to(next_child_up.position)
+	
+	set_rotation(direction.angle())
 
 func _physics_process(delta):
-	var parent = get_parent().get_child(self.get_position_in_parent() - 1)
-	#get the child before me and make its position my parent's postion
-	parent_pos = parent.position 
+	var next_child_up = get_parent().get_child(self.get_position_in_parent() - 1)
+	var dist_to_parent = position.distance_to(next_child_up.position)
+	var direction = position.direction_to(next_child_up.position)
 	
-	pos = position
-	direction = pos.direction_to(parent_pos)
-
-	var dist_to_parent = pos.distance_to(parent_pos)
-	#if my distance from parent is greater than GAP: change velocity to move me towards parent
 	if dist_to_parent > GAP:
-		velocity = direction.normalized()
-		set_rotation(direction.angle())
+		var velocity = direction.normalized()
 		var speed_multiplier = dist_to_parent - GAP if dist_to_parent - GAP >= 1 else 1 
-		print("multi " + str(speed_multiplier))
+		
+		set_rotation(direction.angle())
 		velocity = move_and_slide(velocity * (get_parent().speed * speed_multiplier))
 	
 func hit():
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,8 @@
 extends Node
 
 var tail = preload("res://scenes/Tail.tscn")
-
-#const SPEED_FACTOR = 1.1
 var speed  = 80
 var score = 0
-
-#func _ready():
-#	_update_speed()
-#
-#func _physics_process(delta):
-#	_update_speed()
-	
 	
 func _on_Collectable_collected(value):
 	score += value
@@ -24,6 +15,3 @@ func add_tail():
 	new_tail.position = last_tail.position + offset
 	speed += 3
 	add_child(new_tail)
-
-#func _update_speed():
-#	speed = get_child_count() * SPEED_FACTOR if get_child_count() > 4  else 4 * SPEED_FACTOR
