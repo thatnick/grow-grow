4de2a1b896f70a5941c9f8be79ccbfdada52dc06
Alistair <32274829+plyval@users.noreply.github.com>
2021-05-02 16:45:59 +0100

    Updates to HUD so it tells players how many levels and how many tails they need

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -4,7 +4,10 @@ onready var manager = get_node("/root/Manager")
 
 func _ready():
 	$Tails.text = str(manager.tails)
+	$Goal.text = str(manager.GOAL)
 	$LvlNum.text = str(manager.lvl)
+	$LvlTotal.text = str(manager.LVLTOTAL)
+	
 	
 func _physics_process(_delta):
 	$Tails.text = str(manager.tails)
