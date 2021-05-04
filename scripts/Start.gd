f6e6181bd8b014834d42c95a0707d43c7fd4db23
Nicholas Russell <nick@spirited.software>
2021-05-04 17:04:07 +0100

    Press space to start / restart game (required scripts for previous commit to work).

---
+++ scripts/Start.gd
@@ -0,0 +1,8 @@
+extends Control
+class_name Start
+
+onready var manager: Node = get_node("/root/Manager")
+
+func _process(_delta) -> void:
+	if Input.is_action_pressed("ui_accept"):
+		manager.new_game()
