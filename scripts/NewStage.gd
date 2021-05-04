e5e7f7deb5e0f7df915ef72a5f9557fdaba87ae6
Nicholas Russell <nick@spirited.software>
2021-05-04 17:02:58 +0100

    Fix alignment of text in between level screens. Change colours to match theme. Change input from mouse to keyboard. Remove buttons. Add start image.

--- scripts/NewStage.gd
+++ scripts/NewStage.gd
@@ -1,10 +1,11 @@
 extends Control
+class_name NewStage
 
-var pause = 3
+const PAUSE = 2
 
-func _ready():
-	yield(get_tree().create_timer(pause), "timeout")
+func _ready() -> void:
+	yield(get_tree().create_timer(PAUSE), "timeout")
 	loadlvl()
 
-func loadlvl():
+func loadlvl() -> void:
 	get_tree().change_scene("res://scenes/Main.tscn")
--- scripts/PlayButton.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-onready var manager = get_node("/root/Manager")
-
-func _on_PlayButton_pressed():
-	manager.new_game()
