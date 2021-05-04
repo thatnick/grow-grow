5d9e642d0c80692ef73e287a790b9cfbd1cbac49
Nicholas Russell <nick@spirited.software>
2021-05-04 17:08:07 +0100

    Add types

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,14 +1,15 @@
 extends CanvasLayer
+class_name HUD
 
-onready var manager = get_node("/root/Manager")
+onready var manager: Node = get_node("/root/Manager")
 
-func _ready():
+func _ready() -> void:
 	$Tails.text = str(manager.tails)
 	$Goal.text = str(manager.GOAL)
 	$LvlNum.text = str(manager.lvl)
 	$LvlTotal.text = str(manager.LVLTOTAL)
 	
 	
-func _physics_process(_delta):
+func _physics_process(_delta) -> void:
 	$Tails.text = str(manager.tails)
 	$LvlNum.text = str(manager.lvl)
