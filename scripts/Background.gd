bc76fb8aeb7c8ac59e8e01d66530c2835bd65874
Nicholas Russell <nick@spirited.software>
2021-05-04 17:05:04 +0100

    Add types

--- scripts/Background.gd
+++ scripts/Background.gd
@@ -1,8 +1,9 @@
 extends Sprite
+class_name Background
 
-onready var manager = get_node("/root/Manager")
+onready var manager: Node = get_node("/root/Manager")
 
-export(Array, Texture) var backgrounds
+export(Array, Texture) var backgrounds: Array
 
-func _ready():
+func _ready() -> void:
 	texture = backgrounds[manager.rng.randi_range(0, backgrounds.size() - 1)]
